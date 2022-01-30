0d32fbdbb1886118cee284678e5ee37d13cdbb8f
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2022-01-30 17:10:00 -0300

    feat: chat user input validation (post/participants

--- src/index.js
+++ src/index.js
@@ -2,16 +2,55 @@ import express from "express";
 import cors from "cors";
 import joi from "joi";
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import dayjs from "dayjs";
 
-const server = express();
+function statusMessage(name, status){
+    return {
+        from: name,
+        to: 'Todos',
+        text: `${status} na sala...`,
+        type: 'status',
+        time: dayjs().locale('pt-br').format('HH:mm:ss')
+    }
+}
 
 dotenv.config();
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+const server = express();
 server.use(cors())
 server.use(express.json())
 
-server.post("/participants", (req, res) => {
-    res.send("post participants OK")
+const usernameSchema = joi.object({
+    name: joi.string().min(1).required()
+});
+
+server.post("/participants", async (req, res) => {
+    const username = req.body;
+    try {
+        await mongoClient.connect()
+        const participants = mongoClient.db("chat_UOL").collection("participants");
+        const messages = mongoClient.db("chat_UOL").collection("messages");
+        
+        const validationUsername = usernameSchema.validate(username, { abortEarly: false });
+        const validationOnline = await participants.find({}).toArray()
+
+        if (validationUsername.error || validationOnline.map(participant => participant.name).includes(username.name)) {
+            return res.status(422).send(validationUsername.error? validationUsername.error.details : "Há um usuário online com este nome.");
+        } else {
+            await participants.insertOne({...username, lastStatus: Date.now()})
+            await messages.insertOne(statusMessage(username.name, "entra"))
+        }
+
+        res.sendStatus(201)
+        mongoClient.close()
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500)
+        mongoClient.close();
+    }
 })
 
 server.get("/participants", (req, res) => {
